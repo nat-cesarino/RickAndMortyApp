040982553962ab62d2afbcc36d6553bf2393c0b7
Nathalie Cesarino <nathalie.cesarino@kroton.tech>
2024-04-16 22:18:45 +0200

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Nathalie Pires cesarino on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
